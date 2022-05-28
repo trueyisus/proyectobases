9eacad08bfc702e6f32b54b1290866d0d4074763
Luis M <lems45@outlook.com>
2022-05-28 05:43:05 -0500

    Editar Autobus

--- Autobus/editarAutobus.php
+++ Autobus/editarAutobus.php
@@ -5,6 +5,6 @@
     $placas = $_POST["placas"];
     $modelo = $_POST["modelo"];
 
-	$query = "UPDATE bdii.autobus SET placas = '$placas', modelo = '$modelo' WHERE numero_serie = $idAutobus";
+	$query = "UPDATE bdii.autobus SET placas = '$placas', modelo = '$modelo' WHERE numero_serie = '$idAutobus'";
 	$result = pg_query($dbconn, $query);
 ?>
