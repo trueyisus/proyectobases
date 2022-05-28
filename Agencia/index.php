ef4535db3a1b8f27d68b7510f3c8114aefa835a9
Luis M <lems45@outlook.com>
2022-05-28 03:24:14 -0500

    Editar Agencia

--- Agencia/index.php
+++ Agencia/index.php
@@ -366,7 +366,7 @@ $(document).ready(function(){
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-primary" data-dismoss="modal" id="btnGuardarEditAgencia">Guardar Cambios</button>
+                    <button type="button" class="btn btn-primary" data-dismiss="modal" id="btnGuardarEditAgencia">Guardar Cambios</button>
                 </div>
             </div>
         </div>
