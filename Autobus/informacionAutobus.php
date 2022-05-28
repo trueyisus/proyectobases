de5dfb9666ea0866a29da71d251536523f356d6c
Luis M <lems45@outlook.com>
2022-05-27 23:57:05 -0500

    Interfaz Autobus

--- Autobus/informacionAutobus.php
+++ Autobus/informacionAutobus.php
@@ -2,7 +2,7 @@
     include("../database/conexion.php");
     $idAuto = $_POST["idAuto"];
 
-    $result = pg_query($dbconn, "SELECT * FROM bdii.autobus, bdii.chofer WHERE bdii.chofer.id_chofer = bdii.autobus.id_chofer AND bdii.autobus.numero_serie = $idAuto");
+    $result = pg_query($dbconn, "SELECT * FROM bdii.autobus, bdii.chofer WHERE bdii.chofer.id_chofer = bdii.autobus.id_chofer AND bdii.autobus.numero_serie = '$idAuto'");
     $renglon = pg_fetch_row($result);
 
     echo '
@@ -10,31 +10,31 @@
         <tbody>
             <tr>
                 <th scope="row">Numero de serie: </th>
-                <td>'.$renglon[2].'</td>
+                <td>'.$renglon[0].'</td>
             </tr>
             <tr>
                 <th scope="row">Id chofer: </th>
-                <td>'.$renglon[3].'</td>
+                <td>'.$renglon[1].'</td>
             </tr>
             <tr>
                 <th scope="row">Placas: </th>
-                <td colspan="2">'.$renglon[4].'</td>
+                <td colspan="2">'.$renglon[2].'</td>
             </tr>
             <tr>
                 <th scope="row">Modelo: </th>
-                <td colspan="2">'.$renglon[5].'</td>
+                <td colspan="2">'.$renglon[3].'</td>
             </tr>
             <tr>
                 <th scope="row">ID empleado: </th>
-                <td colspan="2">'.$renglon[7].'</td>
+                <td colspan="2">'.$renglon[5].'</td>
             </tr>                           
             <tr>
                 <th scope="row">Numero de licencia: </th>
-                <td colspan="2">'.$renglon[8].'</td>
+                <td colspan="2">'.$renglon[6].'</td>
             </tr>
             <tr>
                 <th scope="row">Expiracion de licencia: </th>
-                <td colspan="2">'.$renglon[9].'</td>
+                <td colspan="2">'.$renglon[7].'</td>
             </tr>
         </tbody>
     </table>
